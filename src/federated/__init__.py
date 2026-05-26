9407f51735b8b7243b113d3f2bbdcf30ed16c6cf
nav6x <nav6x@users.noreply.github.com>
2026-05-26 14:00:00 +1000

    feat(fed): define federated client interface and local epoch trainer

---
+++ src/federated/__init__.py
@@ -0,0 +1,9 @@
+from .privacy import DifferentialPrivacyManager
+from .client import FederatedClient
+from .server import FederatedServer
+
+__all__ = [
+    "DifferentialPrivacyManager",
+    "FederatedClient",
+    "FederatedServer"
+]
